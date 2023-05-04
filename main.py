b8603de385bd28077949d0b89a92332a5e897313
Paul Gradie <paul.e.gradie@gmail.com>
2023-05-05 08:11:12 +1000

    refactor to functions

--- main.py
+++ main.py
@@ -3,55 +3,83 @@ from dotenv import dotenv_values
 import os
 import openai
 import json
+from typing import Optional
+import argparse
 
-config = dotenv_values(".env") # take environment variables from .env.
-if os.path.exists(".env.development"):
-    config = dotenv_values(".env.development")
+def generate_quote(prompt: str):
+    # Get an interesting quote from chatgpt
+    completion = openai.Completion.create(
+        model="text-davinci-003", 
+        max_tokens=500,
+        temperature=0.7,
+        presence_penalty=0.6,
+        prompt=prompt)
+    inspirational_quote = completion.choices[0].text
+    return inspirational_quote
 
-openAiApiKey = config["OPENAI_API_KEY"]
-linkedInAuthKey = config["LINKEDIN_APIKEY"]
-linkedin_user_id = config["LINKEDIN_USER_ID"]
-openai.api_key = openAiApiKey
+def clean_text(text: str):
+    return text.strip().lstrip("\"").rstrip("\"")
 
-# Get an interesting quote from chatgpt
-completion = openai.Completion.create(
-    model="text-davinci-003", 
-    max_tokens=500,
-    temperature=0.7,
-    presence_penalty=0.6,
-    prompt="Write a 125 word inspirational quote for linkedIn for an audience of people struggling to find a job that is sure to get a big response")
-inspirational_quote = completion.choices[0].text
-
-clean_inspirational_quote = inspirational_quote.strip().lstrip("\"").rstrip("\""))
-print(clean_inspirational_quote)
-
-# Post the quote to LinkedIn
-linked_headers = {
-    "Authorization": f"Bearer {linkedInAuthKey}",
-    "Content-Type": "application/json",
-    "x-li-format": "json"
-}
-data = json.dumps({
-    "author": f"urn:li:person:{linkedin_user_id}",
-    "lifecycleState": "PUBLISHED",
-    "specificContent": {
-        "com.linkedin.ugc.ShareContent": {
-            "shareCommentary": {
-                "text": clean_inspirational_quote
-            },
-            "shareMediaCategory": "NONE"
-        }
-    },
-    "visibility": {
-        "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
+def post_to_linkedin(post_text: str, user_id: str, bearer_token: str, img: Optional[str] = None):
+        
+    # Post the quote to LinkedIn
+    linked_headers = {
+        "Authorization": f"Bearer {bearer_token}",
+        "Content-Type": "application/json",
+        "x-li-format": "json"
     }
-})
+    data = json.dumps({
+        "author": f"urn:li:person:{user_id}",
+        "lifecycleState": "PUBLISHED",
+        "specificContent": {
+            "com.linkedin.ugc.ShareContent": {
+                "shareCommentary": {
+                    "text": post_text
+                },
+                "shareMediaCategory": "NONE"
+            }
+        },
+        "visibility": {
+            "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
+        }
+    })
 
-res = requests.request(
-    "POST",
-    "https://api.linkedin.com/v2/ugcPosts",
-    headers=linked_headers,
-    data=data
-)
+    res = requests.request(
+        "POST",
+        "https://api.linkedin.com/v2/ugcPosts",
+        headers=linked_headers,
+        data=data
+    )
 
-print(res)
+    print(res)  
+
+def main(send_to_linkedin: bool, user_id: Optional[str] = None, bearer_token: Optional[str] = None):
+
+    inspirational_quote = clean_text(generate_quote())
+    print(inspirational_quote)
+
+    if (send_to_linkedin and user_id and bearer_token):
+        post_to_linkedin(inspirational_quote, user_id=user_id, bearer_token=bearer_token)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+                    prog='Cat Pirate Quotes',
+                    description='Uses chatGPT to generate text and post it somewhere',
+                    epilog='Use with caution - who knows what chatGPT will say!')
+
+    parser.add_argument('-l', '--post', action='store_true')
+    parser.add_argument('-p', '--prompt', default="Write a 125 word inspirational quote for linkedIn for an audience of people struggling to find a job that is sure to get a big response")
+    args = parser.parse_args();
+
+    config = dotenv_values(".env") # take environment variables from .env.
+    if os.path.exists(".env.development"):
+        config = dotenv_values(".env.development")
+
+    linkedin_user_id = config["LINKEDIN_USER_ID"]
+    linkedin_bearer_token = config["LINKEDIN_APIKEY"]
+    openAiApiKey = config["OPENAI_API_KEY"]
+    openai.api_key = openAiApiKey
+    
+    main(args.post, linkedin_user_id, linkedin_bearer_token)
